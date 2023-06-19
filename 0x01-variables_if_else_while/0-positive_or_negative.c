f3c2b9cfcdcaebcf3fa8e930b002c924e1915a0b
TshegoJ <jongwetshegofatso@gmail.com>
2023-06-18 21:24:45 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (successs)
  */
-
 int main(void)
 {
 	int n;
@@ -17,7 +15,7 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else 
+	else
 		printf("%d is negative\n", n);
 	return (0);
-}	      	
+}	
